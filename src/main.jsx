e7ba616ccca1ee4a12a2ec5daa762031fcf85bde
lucarl07 <junitinho2007@gmail.com>
2024-04-19 11:45:42 -0300

    Set the App's background and page alignment

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-/* CodeNet - Main Application */
+/* Main Application */
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './components/App/App'
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1>Olá mundo!</h1>
+    <App/>
   </React.StrictMode>,
 )
