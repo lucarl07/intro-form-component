42ecec67e05df4432b7fe8ad95f6a952e6b60cbf
lucarl07 <junitinho2007@gmail.com>
2024-04-19 11:41:00 -0300

    Set global presets and delete unnecessary files

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
+/* CodeNet - Main Application */
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <p>Olá mundo!</p>
+    <h1>Olá mundo!</h1>
   </React.StrictMode>,
 )
